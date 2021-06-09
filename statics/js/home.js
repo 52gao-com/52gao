6ebfbfb6e343047a41dbb0228414671f6553101a
52gao <85625735+52gao@users.noreply.github.com>
2021-06-09 23:45:50 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -9,7 +9,9 @@
         document.getElementById("btnEnter3").onclick = function() {
             window.open('http://52gao3.com', "_self");
         }
-       
+        document.getElementById("btnEnter4").onclick = function() {
+            window.open('http://52gao3.com', "_self");
+        }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('http://www.baidu.com/', "_blank");
         }
