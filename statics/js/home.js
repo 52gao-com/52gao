3970088421d809da487bb69222094a78bd00308e
52gao <85625735+52gao@users.noreply.github.com>
2021-06-09 23:50:28 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -4,10 +4,10 @@
             window.open('http://52gao1.com', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('http://52gao2.com', "_self");
+            window.open('http://52gao1.com', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('http://52gao3.com', "_self");
+            window.open('http://52gao2.com', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
             window.open('http://52gao3.com', "_self");
