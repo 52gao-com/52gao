ea4b3399fc8a87b4de7ed54b5e4cd2b94bd95ee3
52gao <85625735+52gao@users.noreply.github.com>
2021-06-11 17:25:46 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://52gao1.com', "_self");
+            window.open('https://www.52gao1.com', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://52gao2.com', "_self");
+            window.open('https://www.52gao2.com', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://52gao3.com', "_self");
+            window.open('https://www.52gao3.com', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://52gao4.com', "_self");
+            window.open('https://www.52gao4.com', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
-            window.open('https://www.baidu.com/', "_blank");
+            window.open('http://www.baidu.com/', "_blank");
         }
     }
     function init() {
