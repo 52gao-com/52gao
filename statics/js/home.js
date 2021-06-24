22ccaaa5c0bebc2ec54919cf172a5803a1dfa564
52gao <85625735+52gao@users.noreply.github.com>
2021-06-24 16:59:56 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -7,7 +7,7 @@
             window.open('https://www.52gao2.com', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('http://www.52gao3.com', "_self");
+            window.open('https://www.52gao3.com', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
             window.open('https://www.52gao4.com', "_self");
