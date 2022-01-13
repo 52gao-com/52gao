712625d61f7c71fed5bab74b61020d9d926130e8
52gao-com <85625735+52gao-com@users.noreply.github.com>
2022-01-13 15:40:57 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,16 +1,16 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://www.52gao0.com', "_self");
+            window.open('https://www.52gao5.com', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://www.52gao1.com', "_self");
+            window.open('https://www.52gao6.com', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://www.52gao3.com', "_self");
+            window.open('https://www.52gao7.com', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://www.52gao4.com', "_self");
+            window.open('https://www.52gao8.com', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('http://www.baidu.com/', "_blank");
