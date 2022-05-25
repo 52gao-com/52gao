c3f7a532faa9baedd241d1ae84789a6a4c904cf2
52gao-com <85625735+52gao-com@users.noreply.github.com>
2022-05-25 22:46:12 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,7 +1,7 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://www.52gao5.com', "_self");
+            window.open('https://www.52gao6.com', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
             window.open('https://www.52gao6.com', "_self");
